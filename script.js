c1acd9a475b9af4ad200825eeeb1d2ba6d2c8ab2
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2026-05-13 02:01:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-//your code here!
+const list = document.getElementById("infi-list");
+
+function addItems(nums){
+	let count =0;
+
+	for(let i=0;i<nums;i++){
+		count++;
+
+		const li = document.createElement("li");
+
+		li.textContent = `Item ${count}`;
+		list.appendChild(li);
+	}
+}
+
+addItems(nums);
+
+list.addEventListener('scroll', () =>{
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
+		addItems(2);
+	}
+})
+
+
 
